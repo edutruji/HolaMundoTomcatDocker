59a1b9c409eda68087635ca061e1f3e91e9c5870
edutrujillo <eduardo.trujillo.sanchez@everis.com>
2015-11-26 15:39:16 +0100

    primer commit

---
+++ HolaMundoTomcatDocker/src/edutrujillo/Hello.java
@@ -0,0 +1,11 @@
+package edutrujillo;
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        
+            System.out.println("hello from edutruji HelloWorld");
+        
+    }
+}
